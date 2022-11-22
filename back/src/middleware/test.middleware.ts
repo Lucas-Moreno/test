de15919376aaa16d496945409a6706100d3b20d9
Benjamin Faucheux <benjaminfaucheux40@gmail.com>
2022-11-22 09:42:49 +0100

    chore(config): Logger import path refactored

--- back/src/middleware/test.middleware.ts
+++ back/src/middleware/test.middleware.ts
@@ -1,5 +1,5 @@
 import {NextFunction, Request, Response} from "express";
-import logger from "~/logger/logger";
+import logger from "../logger/logger";
 
 export default function testMiddleware(req:Request, res:Response, next:NextFunction) {
     logger.debug("Test middleware OK")
